47247b17ec014e51975b275fbef13a10a4481520
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-21 15:37:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
- 		    :known_for => "Ruby",
- 		    :languages => ["LISP", "C"]
- 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -85,7 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+programmer_hash
 end
 
 def changing_alan
